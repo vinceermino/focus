366733310c6fab701c27f83bd1d24b58daad4373
Vince Ferreras Ermino <vinceferrerasermino25@gmail.com>
2026-09-10 17:36:00 +0800

    fix the build issue

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "../generated/prisma/index.js"
+import { PrismaClient } from "@prisma/client"
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -20,4 +20,4 @@ if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
 }
 
-export * from "../generated/prisma/index.js"
+export * from "@prisma/client"
